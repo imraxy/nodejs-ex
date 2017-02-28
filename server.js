016400b6fd5b08d7741b962253af8c8ff6605362
raxy <imraxy@gmail.com>
2017-02-28 16:16:42 +0530

    added uppercase to service name for mongo environmental variables

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ Object.assign=require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+var port = process.env.PORT || process.env.NODEJS_MONGO_PERSISTENT_SERVICE_PORT || 8080,
+    ip   = process.env.IP   || process.env.NODEJS_MONGO_PERSISTENT_SERVICE_HOST || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
 console.log(process.env.DATABASE_SERVICE_NAME);
 
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME,
+  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
       mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
       mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
       mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
