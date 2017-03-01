e2f64b51a6112c6a80f67160a7a65687cd38e4de
raxy <imraxy@gmail.com>
2017-03-01 18:14:57 +0530

    added more logging in server.js

--- server.js
+++ server.js
@@ -85,6 +85,7 @@ app.get('/', function (req, res) {
 app.get('/news', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
+  console.log("in /news");
   if (!db) {
     initDb(function(err){});
   }  
