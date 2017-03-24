251e976bbefbd6e1051a4936904c98ba8037ea93
raxy <imraxy@gmail.com>
2017-03-24 19:42:12 +0530

    moving to heroku

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL || process.env.MONGODB_URI,
     mongoURLLabel = "";
 
 console.log(process.env.DATABASE_SERVICE_NAME);
